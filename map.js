57914933ee1986c5234cb63ac9ed4504d08f208a
xi-xvii <omarmachucabell@gmail.com>
2025-05-27 16:13:44 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,12 +1,21 @@
 console.log("▶️ map.js loaded");
 
-// 1) Initialize map and set view to London
-var map = L.map('map').setView([51.505, -0.09], 13);
-console.log("🗺️ map initialized at London");
+// 1) Set image dimensions
+const imgW = 11000;
+const imgH = 11000;
 
-// 2) Add OpenStreetMap tile layer
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-console.log("🧱 OSM tile layer added");
+// 2) Define bounds in pixel space (bottom-left origin)
+const bounds = [[0, 0], [imgH, imgW]];
+
+// 3) Init map using L.CRS.Simple
+const map = L.map('map', {
+  crs: L.CRS.Simple,
+  minZoom: 0,
+  maxZoom: 5
+}).fitBounds(bounds);
+
+console.log("🗺️ map initialized and fit to image bounds");
+
+// 4) Add the image as an overlay
+L.imageOverlay('map.png', bounds).addTo(map);
+console.log("🖼️ imageOverlay added");
