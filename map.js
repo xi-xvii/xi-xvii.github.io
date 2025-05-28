835bfe1723156cbad9716ca3f4d36790f47b69e6
xi-xvii <omarmachucabell@gmail.com>
2025-05-27 17:04:43 -0700

    Update map.js

--- map.js
+++ map.js
@@ -4,22 +4,21 @@ const imgW = 11000;
 const imgH = 11000;
 const tileSize = 256;
 const maxZoom = 8;
-const scale = Math.pow(2, maxZoom);
+const tileCount = 1 << maxZoom;
 
-const tileCount = {
-  x: Math.ceil(imgW / tileSize),
-  y: Math.ceil(imgH / tileSize)
-};
+const scaledW = imgW / tileSize;
+const scaledH = imgH / tileSize;
+const mapW = tileCount * tileSize;
+const mapH = tileCount * tileSize;
 
-const scaledW = imgW / scale;
-const scaledH = imgH / scale;
-const bounds = [[0, 0], [scaledH, scaledW]];
+// Leaflet pixel bounds (in CRS.Simple space)
+const bounds = [[0, 0], [mapH, mapW]];
 
 const map = L.map('map', {
   crs: L.CRS.Simple,
   minZoom: 0,
   maxZoom: maxZoom
-}).setView([scaledH / 2, scaledW / 2], maxZoom);
+}).fitBounds(bounds);
 
 console.log("🗺️ map initialized");
 
@@ -28,8 +27,10 @@ L.tileLayer('', {
   minZoom: 0,
   maxZoom: maxZoom,
   noWrap: true,
+  bounds: bounds,
   getTileUrl: function (coords) {
-    const yFlipped = (1 << coords.z) - 1 - coords.y;
+    const maxY = (1 << coords.z) - 1;
+    const yFlipped = maxY - coords.y;
     const url = `tiles/${coords.z}/${coords.x}/${yFlipped}.png`;
     console.log("🧭 requesting tile:", url);
     return url;
